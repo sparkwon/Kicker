afcbac3791ec718b6dfefafe11b82629dd4728a2
Unknown <sp4008@nyu.edu>
2018-02-22 17:35:26 -0500

    Movement and Camera
    added movement and basic camera into the game

---
+++ kiss_n_kick/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	[SerializeField]
+	private float xMax; //maximum value of x when going right
+	[SerializeField]
+	private float yMax;
+	[SerializeField]
+	private float xMin;
+	[SerializeField]
+	private float yMin;
+
+	private Transform target;
+
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.Find ("indian 1_5").transform;	//reference for the player's position
+	}
+	
+	// LateUpdate to make sure the player has moved
+	void LateUpdate () 
+	{
+		transform.position = new Vector3 (Mathf.Clamp (target.position.x, xMin, xMax),
+			Mathf.Clamp (target.position.y, yMin, yMax), transform.position.z);
+	}
+}
---
+++ kiss_n_kick/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+
+	[SerializeField]
+	private float movementSpeed;
+
+	private bool facingRight;
+
+	public bool nearBall;
+
+	void Start()
+	{
+		facingRight = true;	//starting off facing right
+		nearBall = false;
+		myRigidbody = GetComponent<Rigidbody2D> (); 
+	}
+
+	void FixedUpdate()
+	{
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		HandleMovement (horizontal);
+
+		Flip (horizontal);
+	}
+
+	private void HandleMovement(float horizontal)
+	{
+		myRigidbody.velocity = new Vector2 (horizontal * movementSpeed, myRigidbody.velocity.y);
+	}
+
+	private void Flip (float horizontal)	//for looking the correct direction while walking
+	{
+		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) 
+		{
+			facingRight = !facingRight;	
+
+			Vector3 theScale = transform.localScale; //multplying scale with -1 to change direction
+			theScale.x *= -1; 
+			transform.localScale = theScale;
+		}
+	}
+
+	private void Kick ()
+	{
+		
+	}
+}
+

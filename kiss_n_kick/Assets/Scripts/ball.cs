6a1f52abff25bb3a1d89c10e050a3f7ab63127ef
Unknown <sp4008@nyu.edu>
2018-02-23 12:02:13 -0500

    Spring and collision
    Work in progress for kicking mechanic

---
+++ kiss_n_kick/Assets/Scripts/ball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public Rigidbody2D hook;
+
+	public float releaseTime=.15f;
+	public float maxDragDistance = 2f;
+
+	private bool isPressed = false;
+
+	void Update()
+	{
+		if (isPressed) 
+		{
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+			if (Vector3.Distance (mousePos, hook.position) > maxDragDistance) {
+				rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+			}
+			else{
+			rb.position = mousePos;	//conver mouse position into world points
+			}
+		}
+	}
+
+	void OnMouseDown ()
+	{
+		isPressed = true;
+		rb.isKinematic = true;
+	}
+
+	void OnMouseUp()
+	{
+		isPressed = false;
+		rb.isKinematic = false;
+
+		StartCoroutine (Release ());
+	}
+
+	IEnumerator Release ()
+	{
+		yield return new WaitForSeconds(releaseTime);
+
+		GetComponent<SpringJoint2D> ().enabled = false;
+
+		this.enabled = false;
+	}
+
+}

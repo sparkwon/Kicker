aac7bbea5f8401338814297569f36bbafd40bd28
Unknown <sp4008@nyu.edu>
2018-04-18 04:02:50 -0400

    Intro levels
    Added 4 intro levels and level transitions

---
+++ kiss_n_kick/Assets/Scripts/level_control.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class level_control : MonoBehaviour {
+
+	public int index;
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+		if (col.CompareTag ("Player")) 
+		{
+			SceneManager.LoadScene (index); 
+		}
+	}
+}

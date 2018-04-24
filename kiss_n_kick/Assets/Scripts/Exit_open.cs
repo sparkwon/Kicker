803a290390315fdb10ae0cd4c14aab53737c9857
Unknown <sp4008@nyu.edu>
2018-04-23 23:17:15 -0400

    Added functioning exit door
    Added animation for exit door

---
+++ kiss_n_kick/Assets/Scripts/Exit_open.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit_open : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D c) {
+		if (c.gameObject.tag == "Player") {
+			anim.SetTrigger ("open");
+		}
+	}
+}
